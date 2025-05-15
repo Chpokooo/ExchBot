b2f083fcfe7bee21d01ef86950ea3950e64e48b3
Misha bu <mishaataman2011@gmail.com>
2025-05-15 18:38:03 +0200

    added telegram bot logics

---
+++ src/main.js
@@ -0,0 +1,50 @@
+import { Telegraf } from "telegraf"
+import * as dotenv from "dotenv"
+dotenv.config()
+
+const BASE_URL = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}`
+
+const freeBot = []
+bot.start((ctx) => ctx.reply("Welcome"))
+
+bot.help((ctx) => {
+    console.log(ctx.message)
+    ctx.reply("I don`t know")
+})
+
+bot.command("hello", (ctx) => {
+    ctx.reply("Hello")
+})
+
+bot.command("name", (ctx) => {
+    ctx.reply(ctx.chat.first_name + ctx.chat.last_name || "")
+})
+
+bot.command("exch", async (ctx) => {
+    const message = ctx.message.text
+    const [_, sum, from, to] = message.split(" ")
+
+    const response = await fetch(BASE_URL+`/latest/USD`)
+    const { conversion_rates} = await response.json
+
+    ctx.reply(conversion_rates[to] * sum)
+})
+
+
+bot.on("message", (ctx) => {
+
+    if (!freeBot.includes(ctx.chat.id)) {
+        if (ctx.message.text.includes("дякую")) {
+            ctx.reply("Ти випустив мене на свободу")
+            freeBot.push(ctx.chat.id)
+        }
+        ctx.reply(ctx.message.text)
+    }
+})
+
+
+
+bot.launch(() => {
+    console.log("Bot started")
+})
+
